f0059c91246340583b4ec034142a18477b1a5d91
eashtian <eashtian@EASHTIAN-MOBL1.amr.corp.intel.com>
2015-04-25 02:15:54 -0700

    remove /*global*/

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 /*jslint node:true, vars:true, bitwise:true, unparam:true */
 /*jshint unused:true */
-/*global */
 
 /*
 A simple node.js application intended to write data to Digital pins on the Intel based development boards such as the Intel(R) Galileo and Edison with Arduino breakout board.
