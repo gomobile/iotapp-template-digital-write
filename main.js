d0f45d2404cb67f19b66ba8bd885ee7018632067
Paul Fischer <paul.a.fischer@intel.com>
2017-03-20 15:48:20 -0700

    Rewrite cfg-app-platform.js to return an object constructor.
    Made this change because it is a more commonly used construct and makes it easier to understand how to use this module. Also allows for easier reuse the module as a mechanism for working with multiple I/O objects.

--- main.js
+++ main.js
@@ -25,14 +25,17 @@
 
 
 var APP_NAME = "IoT Digital Write" ;
-var cfg = require("./utl/cfg-app-platform.js")() ;      // init and config I/O resources
+var Cfg = require("./utl/cfg-app-platform.js") ;    // get Cfg() constructor
+var cfg = new Cfg() ;                               // init and config I/O resources
 
-console.log("\n\n\n\n\n\n") ;                           // poor man's clear console
+console.log("\n\n\n\n\n\n") ;                       // poor man's clear console
 console.log("Initializing " + APP_NAME) ;
 
-process.on("exit", function(code) {                     // define up front, due to no "hoisting"
+process.on("exit", function(code) {                 // define up front, due to no "hoisting"
     clearInterval(intervalID) ;
-    console.log("\nExiting " + APP_NAME + ", with code:", code) ;
+    console.log(" ") ;
+    console.log("Exiting " + APP_NAME + ", with code:", code) ;
+    console.log(" ") ;
 }) ;
 
 
